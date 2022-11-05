17bde5a550aec728a761de266b8c564bc6678eb1
nguyenthienphu <98658191+nguyenthienphu@users.noreply.github.com>
2022-11-05 20:27:28 +0700

    demo

---
+++ website/saleapp/utils.py
@@ -0,0 +1,17 @@
+import json,os
+from saleapp import app
+
+def read_json(path):
+    with open(path,"r") as f:
+        return json.load(f)
+
+def load_categories():
+    return read_json(os.path.join(app.root_path,'data/categories.json'))
+
+def load_products(cate_id = None):
+    products  = read_json(os.path.join(app.root_path,'data/products.json'))
+
+    if cate_id:
+        products = [p for p in products if p['category_id'] == int(cate_id)]
+
+    return products
