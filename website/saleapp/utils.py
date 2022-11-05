66854ff4ab8778a635f9c28622138f49c8f9a6fe
nguyenthienphu <98658191+nguyenthienphu@users.noreply.github.com>
2022-11-06 01:11:59 +0700

    demo

--- website/saleapp/utils.py
+++ website/saleapp/utils.py
@@ -8,10 +8,22 @@ def read_json(path):
 def load_categories():
     return read_json(os.path.join(app.root_path,'data/categories.json'))
 
-def load_products(cate_id = None):
+def load_products(cate_id = None,kw=None,from_price=None,to_price=None):
     products  = read_json(os.path.join(app.root_path,'data/products.json'))
 
     if cate_id:
         products = [p for p in products if p['category_id'] == int(cate_id)]
-
+    if kw:
+        products = [p for p in products if p['name'].lower().find(kw.lower()) >= 0]
+    if from_price:
+        products = [p for p in products if p['price'] >= float(from_price)]
+    if to_price:
+        products = [p for p in products if p['price'] <= float(to_price)]
     return products
+
+def get_product_by_id(product_id):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+
+    for p in products:
+        if p['id']==product_id:
+            return p
