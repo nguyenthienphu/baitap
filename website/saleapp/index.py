7cd68c6ed782b97625659257d71f4fcfee869d96
nguyenthienphu <98658191+nguyenthienphu@users.noreply.github.com>
2022-11-06 13:02:50 +0700

    demo

--- website/saleapp/index.py
+++ website/saleapp/index.py
@@ -17,14 +17,15 @@ def products_list():
     products =utils.load_products(cate_id=cate_id,
                                   kw=kw,
                                   from_price = from_price,
-                                  to_price = to_price
-                                  )
+                                  to_price = to_price)
 
     return render_template('products.html',products=products)
 
 @app.route("/products/<int:product_id>")
 def product_detail(product_id):
     product=utils.get_product_by_id(product_id)
+
+    return render_template('product_detail.html',product=product)
 
 if __name__ == '__main__':
     app.run(debug=True)
